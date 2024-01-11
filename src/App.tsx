346082ac10d6b5a2ba8a1a444d29e5221fe5a9c8
Tareq Hasan <tareq1988@gmail.com>
2024-01-11 13:38:23 +0600

    Bump version to v0.3.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import { useState } from 'react';
-import { TextField, Textarea } from '.';
-
 const App = () => {
-  const [value, setValue] = useState('');
-
-  return (
-    <div className="p-6 space-y-4">
-      <TextField
-        label="Label"
-        value={value}
-        onChange={(value) => setValue(value)}
-        onBlur={() => console.log('on blur')}
-        onFocus={() => console.log('on focus')}
-      />
-
-      <Textarea
-        label="Label"
-        value={value}
-        onChange={(value) => setValue(value)}
-        onBlur={() => console.log('on blur')}
-        onFocus={() => console.log('on focus')}
-      />
-    </div>
-  );
+  return <div className="p-6 space-y-4"></div>;
 };
 
 export default App;
