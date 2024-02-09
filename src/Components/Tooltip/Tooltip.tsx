43884f8598e35cd6f3794c55f7a60566cc97d5c3
Tareq Hasan <tareq1988@gmail.com>
2024-02-09 12:50:20 +0600

    feat: added tooltip component (#14)

---
+++ src/Components/Tooltip/Tooltip.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { twMerge } from 'tailwind-merge';
+
+interface TooltipProps {
+  content: ReactNode | string;
+  children: React.ReactNode;
+  side?: 'top' | 'bottom' | 'left' | 'right';
+  className?: string;
+  arrowClassName?: string;
+  offset?: number;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+  content,
+  children,
+  side = 'top',
+  offset = 5,
+  className,
+  arrowClassName,
+}) => {
+  return (
+    <TooltipPrimitive.Provider delayDuration={100}>
+      <TooltipPrimitive.Root>
+        <TooltipPrimitive.Trigger>{children}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Content
+          sideOffset={offset}
+          side={side}
+          className="animate-slide-up-fade z-[99] items-center overflow-hidden"
+        >
+          {typeof content === 'string' ? (
+            <span
+              className={twMerge(
+                'block max-w-xs px-2.5 py-1.5 text-center text-xs text-white bg-gray-900 opacity-90 rounded-md shadow-lg',
+                className
+              )}
+            >
+              {content}
+            </span>
+          ) : (
+            content
+          )}
+          <TooltipPrimitive.Arrow className={twMerge('fill-gray-900 opacity-90', arrowClassName)} />
+        </TooltipPrimitive.Content>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
+  );
+};
+
+export default Tooltip;
