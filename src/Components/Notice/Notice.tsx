8115f26b41aa6db16b67f31fd422968af1071172
Tareq Hasan <tareq1988@gmail.com>
2024-02-13 21:47:01 +0600

    feat: dismissable notice, dark mode support (#18)
    * feat: added darkmode support
    
    * feat: dismissable notice
    
    * refactor: Update Tooltip trigger to use asChild prop
    
    * feat: Update Notice component styles for dark mode
    
    * feat: add dismissible option to `Notice` component
    
    * Add support for HTML in SwitchInput help text
    
    * refactor(Modal): Update Modal styling for dark mode

--- src/Components/Notice/Notice.tsx
+++ src/Components/Notice/Notice.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import {
   ExclamationTriangleIcon,
@@ -6,25 +6,27 @@ import {
   CheckCircleIcon,
   InformationCircleIcon,
 } from '@heroicons/react/20/solid';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 export interface NoticeProps {
   label?: React.ReactNode;
   type?: 'success' | 'warning' | 'error' | 'info';
   className?: string;
   children?: React.ReactNode;
+  dismissible?: boolean;
 }
 
 const getNoticeColor = (type: NoticeProps['type']) => {
   switch (type) {
     case 'warning':
-      return 'bg-yellow-50 text-yellow-800 ring-yellow-600/20';
+      return 'bg-yellow-50 text-yellow-800 ring-yellow-600/20 dark:bg-yellow-700 dark:text-yellow-200 dark:ring-yellow-500/50';
     case 'error':
-      return 'bg-red-50 text-red-700 ring-red-600/10';
+      return 'bg-red-50 text-red-700 ring-red-600/10 dark:bg-red-700 dark:text-red-200 dark:ring-red-500/50';
     case 'info':
-      return 'bg-blue-50 text-blue-700 ring-blue-700/10';
+      return 'bg-blue-50 text-blue-700 ring-blue-700/10 dark:bg-blue-700 dark:text-blue-200 dark:ring-blue-500/50';
     case 'success':
     default:
-      return 'bg-green-50 text-green-700 ring-green-600/20';
+      return 'bg-green-50 text-green-700 ring-green-600/20 dark:bg-green-700 dark:text-green-200 dark:ring-green-500/50';
   }
 };
 
@@ -42,17 +44,38 @@ const getIcon = (type: NoticeProps['type']) => {
   }
 };
 
-const Notice: React.FC<NoticeProps> = ({ label, type = 'success', className, children }) => {
+const Notice: React.FC<NoticeProps> = ({
+  label,
+  type = 'success',
+  className,
+  dismissible = false,
+  children,
+}) => {
+  const [isVisible, setIsVisible] = useState(true);
   const color = getNoticeColor(type);
+
+  if (!isVisible) return null;
 
   return (
     <div className={twMerge('rounded-md p-4 ring-1 ring-inset w-full', color, className)}>
-      <div className="flex">
-        <div className="flex-shrink-0">{getIcon(type)}</div>
-        <div className="ml-3">
-          {label && <h3 className="text-sm font-medium">{label}</h3>}
-          {children && <div className="text-sm mt-2">{children}</div>}
+      <div className="flex justify-between">
+        <div className="flex">
+          <div className="flex-shrink-0">{getIcon(type)}</div>
+          <div className="ml-3">
+            {label && <h3 className="text-sm font-medium">{label}</h3>}
+            {children && <div className="text-sm mt-2">{children}</div>}
+          </div>
         </div>
+
+        {dismissible && (
+          <button
+            onClick={() => setIsVisible(false)}
+            className="flex-shrink-0 ml-4"
+            title="Dismiss the notice"
+          >
+            <XMarkIcon className="h-4 w-4 text-gray-500 dark:text-gray-300" aria-hidden="true" />
+          </button>
+        )}
       </div>
     </div>
   );
