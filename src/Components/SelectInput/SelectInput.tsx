4f63f8164cbff27d1fb5de1b5075623ffd224165
Tareq Hasan <tareq1988@gmail.com>
2024-02-14 12:16:50 +0600

    fix: added border for SelectInput dark mode

--- src/Components/SelectInput/SelectInput.tsx
+++ src/Components/SelectInput/SelectInput.tsx
@@ -71,7 +71,7 @@ const SelectInput: React.FC<SelectProps> = ({
       )}
       <select
         className={twMerge(
-          'block w-full dark:bg-white/5 rounded-md border-0 py-1.5 px-2 text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none',
+          'block w-full dark:bg-white/5 rounded-md border-0 py-1.5 px-2 text-gray-900 dark:text-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none',
           className,
           error && 'ring-red-300 text-red-900  placeholder:text-red-300 focus:ring-red-500'
         )}
